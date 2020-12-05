b0ba3b485dd8c028b0e8eface83168651f0aee40
Tim Stokman <timstokman@gmail.com>
2020-12-05 04:38:06 +0100

    Use pattern matching instead of lookup table

--- src/day4.rs
+++ src/day4.rs
@@ -5,27 +5,14 @@ use std::fs::File;
 use regex::Regex;
 use std::io::{self, BufRead};
 
-type PassportValidation<'a> = (&'a str, fn(&str) -> bool);
-static PASSPORT_VALIDATION: &[PassportValidation] = &[
-    ("byr", |s| (1920..=2002).contains(&s.parse::<i64>().unwrap_or(-1))),
-    ("iyr", |s| (2010..=2020).contains(&s.parse::<i64>().unwrap_or(-1))),
-    ("eyr", |s| (2020..=2030).contains(&s.parse::<i64>().unwrap_or(-1))),
-    ("hgt", |s| {
-        let re = Regex::new(r"^([0-9]+)(cm|in)$").expect("should be a valid regex");
-        match re.captures(s) {
-            Some(captures) => {
-                let num = captures[1].parse::<i64>().expect("should be a number at this point");
-                if &captures[2] == "cm" {
-                    (150..=193).contains(&num)
-                } else {
-                    (59..=76).contains(&num)
-                }
-            }, _ => false
-        }
-    }),
-    ("hcl", |s| Regex::new(r"^#[0-9a-f]+$").expect("should be a valid regex").is_match(s)),
-    ("ecl", |s| ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].iter().any(|cont| *cont == s)),
-    ("pid", |s| Regex::new(r"^[0-9]{9}$").expect("should be a valid regex").is_match(s))
+static PASSPORT_KEYS: [&str; 7] = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
 ];
 
 pub fn answer() -> common::BoxResult<(usize, usize)> {
@@ -35,12 +22,37 @@ pub fn answer() -> common::BoxResult<(usize, usize)> {
 }
 
 fn is_valid_passport_simple(passport: &HashMap<String, String>) -> bool {
-    PASSPORT_VALIDATION.iter().map(|k| k.0).all(|k| passport.contains_key(k))
+    PASSPORT_KEYS.iter().all(|k| passport.contains_key(*k))
 }
 
 fn is_valid_passport(passport: &HashMap<String, String>) -> bool {
     is_valid_passport_simple(passport) &&
-    PASSPORT_VALIDATION.iter().all(|validation| validation.1(&passport[validation.0]))
+    PASSPORT_KEYS.iter().all(|key| is_valid_passport_entry(&key, &passport.get::<str>(key).expect("key not found")))
+}
+
+fn is_valid_passport_entry(key: &str, value: &str) -> bool {
+    match key {
+        "byr" => (1920..=2002).contains(&value.parse::<i64>().unwrap_or(-1)),
+        "iyr" => (2010..=2020).contains(&value.parse::<i64>().unwrap_or(-1)),
+        "eyr" => (2020..=2030).contains(&value.parse::<i64>().unwrap_or(-1)),
+        "hgt" => {
+            let re = Regex::new(r"^([0-9]+)(cm|in)$").expect("should be a valid regex");
+            match re.captures(value) {
+                Some(captures) => {
+                    let num = captures[1].parse::<i64>().expect("should be a number at this point");
+                    if &captures[2] == "cm" {
+                        (150..=193).contains(&num)
+                    } else {
+                        (59..=76).contains(&num)
+                    }
+                }, _ => false
+            }
+        },
+        "hcl" => Regex::new(r"^#[0-9a-f]+$").expect("should be a valid regex").is_match(value),
+        "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].iter().any(|cont| *cont == value),
+        "pid" => Regex::new(r"^[0-9]{9}$").expect("should be a valid regex").is_match(value),
+        _ => false
+    }
 }
 
 fn read_input(file: &str) -> common::BoxResult<Vec<HashMap<String, String>>> {
@@ -55,5 +67,5 @@ fn read_passport(lines: &[String]) -> HashMap<String, String> {
          .flat_map(|l| l.split(' '))
          .map(|passport_entry| passport_entry.split(':').collect::<Vec<&str>>())
          .map(|passport_entry| (passport_entry[0].to_string(), passport_entry[1].to_string()))
-         .collect::<HashMap<_, _>>()
+         .collect()
 }
